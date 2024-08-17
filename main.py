744da74f2807a50f0b4b0bf6c2975abec28e9547
Daniel <daniel.graef14@gmail.com>
2024-08-17 14:24:29 +0200

    only show folders

--- main.py
+++ main.py
@@ -15,8 +15,21 @@ def print_file_paths(root_folder, exclude_folders=None):
         for file in files:
             file_path = os.path.join(root, file)
             print(file_path)
+            
+            
+def print_folder_paths(root_folder, exclude_folders=None):
+    for root, dirs, files in os.walk(root_folder):
+        if exclude_folders:
+            dirs[:] = [d for d in dirs if d not in exclude_folders]  # Exclude specified folders from further traversal
+            for exclude_folder in exclude_folders:
+                if exclude_folder in dirs:
+                    print(f"Excluding folder: {exclude_folder}")
+                    dirs.remove(exclude_folder)
+                    
+        print(root)
 
 if __name__ == "__main__":
+    only_folders = input("Only show folders y/n: ")=="y"
     root_folder = input("Enter the root folder path: ").strip()
     
     exclude_folders = []
@@ -24,4 +37,7 @@ if __name__ == "__main__":
     if exclude_input:
         exclude_folders = [folder.strip() for folder in exclude_input.split(",")]
 
-    print_file_paths(root_folder, exclude_folders)
+    if only_folders:
+        print_folder_paths(root_folder, exclude_folders)
+    else:
+        print_file_paths(root_folder, exclude_folders)
